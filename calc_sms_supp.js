6fac14e7d698062d9f8bab99ffd49b9deda44c9f
balatzar <bataprod@gmail.com>
2015-12-09 08:26:26 +0100

    working but so ugly

--- calc_sms_supp.js
+++ calc_sms_supp.js
@@ -8,31 +8,36 @@ var calc = function(nbSMS) {
 }
 
 var main = function(str) {
+  console.log("\n--------\n")
   var answer = [];
   var totalSize = str.length;
-  var nbSMS = Math.ceil(str.length / 160);
-  console.log("nbSMS", nbSMS);
+  var nbSMS = Math.ceil(totalSize / 160);
+  console.log("nbSMS :", nbSMS);
   var charSupp = calc(nbSMS);
-  console.log("charSupp", charSupp)
-  if (Math.floor((str.length + charSupp) / 160) != nbSMS) {
-    charSupp = calc(Math.ceil((str.length + charSupp) / 160));
+  console.log("charSupp :", charSupp)
+  if (Math.ceil((totalSize + charSupp) / 160) != nbSMS) {
+    charSupp = calc(Math.ceil((totalSize + charSupp) / 160));
   }
-  console.log("charSupp2", charSupp)
-  nbSMS = Math.ceil((str.length + charSupp) / 160);
+  console.log("charSupp2 :", charSupp)
+  nbSMS = Math.ceil((totalSize + charSupp) / 160);
+  console.log("nbSMS2 :", nbSMS)
+  console.log("\n--------\n")
   var i = 0;
   var index = 0;
   while (++i <= nbSMS) {
+    console.log("i :", i)
     var strToAdd = " - part " + i + " of " + nbSMS;
-    var j = 0;
-    if (totalSize > 160) {
+    console.log("totalSize :", totalSize)
+    if (totalSize > 160 - strToAdd.length) {
       var stop = 160 - strToAdd.length;
-      totalSize -= 160;
+      totalSize -= (160 - strToAdd.length);
     } else {
-      var stop = totalSize - strToAdd.length;
+      var stop = totalSize;
     }
-    console.log(stop)
+    console.log("stop :", stop)
     var temp = "";
-    while (j++ <= stop) {
+    var j = 0;
+    while (j++ < stop) {
       temp += str[index];
       index++;
     }
@@ -40,10 +45,11 @@ var main = function(str) {
     answer.push(temp);
   }
   console.log(answer);
+  console.log("\n--------\n")
 }
 
 var test = "";
-var i = 2000;
+var i = 300;
 while (i--)
   test += "a";
 
